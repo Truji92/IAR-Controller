9ca1c1b996af4bdfbd3e59068afe938ed170c4d8
César Antonio Enrique Ramirez <caenrique93@gmail.com>
2016-03-12 18:26:34 +0100

    Test de ir hacia delante. FIX

--- src/actuadores/MotorOruga.java
+++ src/actuadores/MotorOruga.java
@@ -1,21 +1,21 @@
 package actuadores;
 
-import serialPort.SerialPort;
+import serialPort.SerialPortController;
 
 /**
  * Controlador del Motor Oruga
  */
 public class MotorOruga {
 
-    private SerialPort serial;
+    private SerialPortController serial;
     static private char direccion = 0xB1;
     static private char modo = 0x00;
     static private char velocidad = 0x01;
     static private char giro = 0x02;
 
 
-    public MotorOruga() {
-        serial = new SerialPort();
+    public MotorOruga(SerialPortController serial) {
+        this.serial = serial;
     }
 
     public void forward() {
