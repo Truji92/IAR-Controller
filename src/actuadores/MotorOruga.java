df5b1737b3aa49637568dec40426a9ab87a66f31
César Antonio Enrique Ramirez <caenrique93@gmail.com>
2016-03-14 17:43:54 +0100

    MotorController listo

--- src/actuadores/MotorOruga.java
+++ src/actuadores/MotorOruga.java
@@ -18,16 +18,34 @@ public class MotorOruga {
         this.serial = serial;
     }
 
-    public void forward() {
-        serial.send(direccion,modo,new char[]{0x02});
+    public void goForward() {
+        motorAction((byte) 0xFF, (byte) 0xFF);
+    }
+
+    public void goBackward() {
+        motorAction((byte) 0x00, (byte) 0x00);
+    }
+
+    public void spinClockwise() {
+        motorAction((byte) 0xFF, (byte) 0x00);
+    }
+
+    public void spinCounterClockwise() {
+        motorAction((byte) 0x00, (byte) 0xFF);
+    }
+
+    public void motorAction(byte rueda1, byte rueda2) {
+         serial.send(direccion,modo,new char[]{0x00});
         if (serial.read() == 0x00) return;
 
-        serial.send(direccion,velocidad,new char[]{0x8F});
+        serial.send(direccion,velocidad,new char[]{(char) rueda1});
         if (serial.read() == 0x00) return;
 
-        serial.send(direccion,giro,new char[]{0x80});
+        serial.send(direccion,giro,new char[]{(char) rueda2});
         if (serial.read() == 0x00) return;
     }
+
+
 
 
 }
