5afbdb022bda2735acab9b253ed4a8480b89be1e
Ztryx <not.andres.diaz@gmail.com>
2016-03-14 17:34:40 +0100

    Cuerpo del controlador Brujula creado

--- src/sensores/Brujula.java
+++ src/sensores/Brujula.java
@@ -1,7 +1,20 @@
 package sensores;
 
+import serialPort.SerialPortController;
+
 /**
  * Controlador de la brújula CMPS09
  */
 public class Brujula {
+    private SerialPortController serial;
+    static private char direccion = 0xC0;
+    static private char rumbo = 0x01;
+
+    public Brujula(SerialPortController serial) {
+        this.serial = serial;
+    }
+
+    public void read(){
+        this.rumbo = serial.read();
+    }
 }
