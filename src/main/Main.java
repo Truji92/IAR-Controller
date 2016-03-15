b9da599a065a49def24b3fd3807b134823c79918
Alejandro Trujillo Caballero <alejandro.trujillo.7@gmail.com>
2016-03-15 18:21:36 +0100

    Main test

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,14 @@
 package main;
 
+import actuadores.MotorOruga;
 import giovynet.nativelink.SerialPort;
+import sensores.Brujula;
 import sensores.SRF;
 import serialPort.SerialPortController;
 
 import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.BrokenBarrierException;
 
 
 /**
@@ -12,37 +16,59 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args)throws Exception {
-        // SerialPortController: es un objeto del tipo de puerto que vamos a utilizar
-        SerialPort free = new SerialPort();
 
-        // Nos permite obtener la lista de puertos disponibles
-        List<String> portsList = free.getFreeSerialPort();
-        System.out.println("Puertos Disponibles");
-
-        // Para verifica los puertos creamos un for mediante el cual
-        // ontendremos los puertos libres impresos en consola
-        for (String String : portsList)
-        {
-            System.out.println(String );
-        }
 
         // Ejemplo de uso la interfaz de envio recepcion
         SerialPortController serial = new SerialPortController();
-        serial.send('a', 'b', new char[]{'1','2'});
-//        while (true) {
-//            // Si conectas COM3 a un hyperterminal, lo que escribas aparecera en la consola
-//            System.out.println(serial.read());
+//        serial.send('a', 'b', new char[]{'1','2'});
+////        while (true) {
+////            // Si conectas COM3 a un hyperterminal, lo que escribas aparecera en la consola
+////            System.out.println(serial.read());
+////        }
+//
+//
+//        //test sensores
+//        SRF[] sensores = SRF.initializeSensors(serial);
+//        int[] medidas = new int[sensores.length];
+//        for (int i = 0; i < sensores.length; i++) {
+//            medidas[i] = sensores[i].medir(); //Acción bloqueante e.e
+//            System.out.println(medidas[i]);
 //        }
 
+        Brujula brujula = new Brujula(serial);
+        MotorOruga ruedas = new MotorOruga(serial);
 
-        //test sensores
-        SRF[] sensores = SRF.initializeSensors(serial);
-        int[] medidas = new int[sensores.length];
-        for (int i = 0; i < sensores.length; i++) {
-            medidas[i] = sensores[i].medir(); //Acción bloqueante e.e
-            System.out.println(medidas[i]);
+        Scanner teclado = new Scanner(System.in);
+        boolean seguir = true;
+
+
+        while (seguir) {
+
+            System.out.println("GIRANDO SENTIDO HORARIO");
+            ruedas.spinClockwise();
+            Thread.sleep(3000);
+
+            System.out.println("GIRANDO SENTIDO ANTIHORARIO");
+            ruedas.spinCounterClockwise();
+            Thread.sleep(3000);
+
+            System.out.println("AVANZANDO");
+            ruedas.goForward();
+            Thread.sleep(3000);
+
+            System.out.println("MARCHA ATRAS");
+            ruedas.goBackward();
+            Thread.sleep(3000);
+
+            ruedas.stop();
+
+            System.out.println("INTRODUCE ALGO PARA REINICIAR EL TEST (o introduce stop para detener)");
+
+            String next = teclado.next();
+            System.out.println(next);
+            if (next.compareTo("stop") == 0) seguir = false;
+            teclado.reset();
         }
-
     }
 
 }
