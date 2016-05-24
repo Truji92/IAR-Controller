0f7e67e0269ad70d1b23612a3214441c0ee7facb
Alejandro Trujillo Caballero <alejandro.trujillo.7@gmail.com>
2016-05-24 19:55:01 +0200

    Camara

--- src/sensores/Camara.java
+++ src/sensores/Camara.java
@@ -1,5 +1,13 @@
 package sensores;
 
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.highgui.Highgui;
+import org.opencv.highgui.VideoCapture;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+
 public class Camara {
 
     public Camara()
@@ -9,29 +17,32 @@ public class Camara {
 
     public static void main(String... args)
     {
-        Webcam wc = Webcam.getDefault();
-        if(wc!=null)
-        {
-            System.out.println(wc.getName());
-        }
-        wc.open();
-        ImageIO.write(wc.getImage(), "PNG", new File("prueba.png"));
+//        Webcam wc = Webcam.getDefault();
+//        if(wc!=null)
+//        {
+//            System.out.println(wc.getName());
+//        }
+//        wc.open();
+//        ImageIO.write(wc.getImage(), "PNG", new File("prueba.png"));
 
-        Cerramos el puerto
-        p.close();
+        //Cerramos el puerto
+        //p.close();
 
 
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         Mat frame = new Mat();
         VideoCapture camera = new VideoCapture(0);
-        Thread.sleep(1000);
+        try {
+            Thread.sleep(1000);
+        } catch (Exception ignored) {}
 
         camera.open(0);
         if (!camera.isOpened()) {
             System.out.println("Error al abrir la camara");
         } else {
             camera.read(frame);
-            Highgui.imwrite("nuevaFoto2.jpg", frame);
+            long time = System.currentTimeMillis();
+            Highgui.imwrite("nuevaFoto2"+time+".jpg", frame);
             camera.release();
 
         }
